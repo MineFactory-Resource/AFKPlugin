2c5b991d78be5005a92c4a225b0b41c262662695
Seokhun-Yang <ddayang1234@gmail.com>
2022-08-21 14:56:08 +0900

    initial commit

---
+++ src/main/java/net/teamuni/afkplugin/AFKPlugin.java
@@ -0,0 +1,17 @@
+package net.teamuni.afkplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AFKPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
