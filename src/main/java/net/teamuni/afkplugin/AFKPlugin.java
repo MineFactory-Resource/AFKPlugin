aa84a9cb42d5f854a691e8f27fcf6d4f747113f2
Seokhun-Yang <ddayang1234@gmail.com>
2022-08-21 17:10:13 +0900

    플레이어가 이미 잠수맵일 경우 잠수맵으로 이동 불가능

--- src/main/java/net/teamuni/afkplugin/AFKPlugin.java
+++ src/main/java/net/teamuni/afkplugin/AFKPlugin.java
@@ -11,7 +11,6 @@ import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Objects;
 
 public final class AFKPlugin extends JavaPlugin implements Listener {
 
@@ -33,17 +32,21 @@ public final class AFKPlugin extends JavaPlugin implements Listener {
         Player player = (Player) sender;
 
         if (cmd.getName().equalsIgnoreCase("잠수") && player.hasPermission("afk.afk")) {
-            World world = Bukkit.getServer().getWorld(Objects.requireNonNull(getConfig().getString("afkpoint.world")));
-            double x = getConfig().getDouble("afkpoint.x");
-            double y = getConfig().getDouble("afkpoint.y");
-            double z = getConfig().getDouble("afkpoint.z");
-            float yaw = (float) getConfig().getDouble("afkpoint.yaw");
-            float pitch = (float) getConfig().getDouble("afkpoint.pitch");
-            player.sendMessage(ChatColor.GOLD + "" + ChatColor.BOLD + "이동 중...");
-            player.teleport(new Location(world, x, y, z, yaw, pitch));
+            if (!player.getLocation().getWorld().getName().equalsIgnoreCase(getConfig().getString("afkpoint.world"))) {
+                World world = Bukkit.getWorld(getConfig().getString("afkpoint.world"));
+                double x = getConfig().getDouble("afkpoint.x");
+                double y = getConfig().getDouble("afkpoint.y");
+                double z = getConfig().getDouble("afkpoint.z");
+                float yaw = (float) getConfig().getDouble("afkpoint.yaw");
+                float pitch = (float) getConfig().getDouble("afkpoint.pitch");
+                player.sendMessage(ChatColor.GOLD + "" + ChatColor.BOLD + "이동 중...");
+                player.teleport(new Location(world, x, y, z, yaw, pitch));
+            } else {
+                player.sendMessage(ChatColor.RED + "이동할 수 없습니다.");
+            }
         }
         if (cmd.getName().equalsIgnoreCase("잠수지역설정") && player.hasPermission("afk.setpoint")) {
-            getConfig().set("afkpoint.world", Objects.requireNonNull(player.getLocation().getWorld()).getName());
+            getConfig().set("afkpoint.world", player.getLocation().getWorld().getName());
             getConfig().set("afkpoint.x", player.getLocation().getX());
             getConfig().set("afkpoint.y", player.getLocation().getY());
             getConfig().set("afkpoint.z", player.getLocation().getZ());
