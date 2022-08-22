9bcb396420dada08e8e644ea25c820f1f7c459ea
Seokhun-Yang <ddayang1234@gmail.com>
2022-08-22 20:21:09 +0900

    불필요한 import 제거

--- src/main/java/net/teamuni/afkplugin/AFKPlugin.java
+++ src/main/java/net/teamuni/afkplugin/AFKPlugin.java
@@ -11,8 +11,6 @@ import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Objects;
-
 
 public final class AFKPlugin extends JavaPlugin implements Listener {
 
