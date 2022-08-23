115c33ea6fac49caf3b03c53d6358354a618355b
Seokhun-Yang <ddayang1234@gmail.com>
2022-08-23 20:37:50 +0900

    /잠수포인트 명령어 뒤에오는 확인, 설정, 지급, 차감 자동완성 되도록 하기

--- src/main/java/net/teamuni/afkplugin/CommandTabCompleter.java
+++ src/main/java/net/teamuni/afkplugin/CommandTabCompleter.java
@@ -3,6 +3,7 @@ package net.teamuni.afkplugin;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -12,13 +13,33 @@ import java.util.List;
 public class CommandTabCompleter implements TabCompleter {
     @Nullable
     @Override
-    public List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String alias, @NotNull String[] args) {
-        if (command.getName().equalsIgnoreCase("afkplugin")) {
+    public List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command cmd, @NotNull String alias, @NotNull String[] args) {
+        Player player = (Player) sender;
+
+        if (cmd.getName().equalsIgnoreCase("afkplugin")) {
             List<String> tabCompleteList = new ArrayList<>();
             if (args.length == 1) {
                 tabCompleteList.add("reload");
             }
             return tabCompleteList;
+        }
+        if (cmd.getName().equalsIgnoreCase("잠수포인트")) {
+            if (player.isOp()) {
+                List<String> tabCompleteList = new ArrayList<>();
+                if (args.length == 1) {
+                    tabCompleteList.add("확인");
+                    tabCompleteList.add("설정");
+                    tabCompleteList.add("지급");
+                    tabCompleteList.add("차감");
+                }
+                return tabCompleteList;
+            } else {
+                List<String> tabCompleteList = new ArrayList<>();
+                if (args.length == 1) {
+                    tabCompleteList.add("확인");
+                }
+                return tabCompleteList;
+            }
         }
         return null;
     }
